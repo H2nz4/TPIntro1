057d6c70ff104ccb19b909336d95659fc67f8ef7
--unset <hamza.akeddar@epfl.ch>
2024-02-22 10:09:14 +0100

    commit

--- src/main.c
+++ src/main.c
@@ -1,16 +1,1 @@
-$ git init # init git. This will typically create a .git folder locally
-$ git status
-On branch main
-
-No commits yet
-
-Untracked files:
-  (use "git add <file>..." to include in what will be committed)
-        src/
-
-nothing added to commit but untracked files present (use "git add" to track)
-$ git add src/ # add all files in src directory to the next commit
-$ git commit -m "first commit" # commit
-$ git status
-On branch main
-nothing to commit, working tree clean
+zqsredfgh
